e5ce01c0b0562438e63c6105379a50c8a7c41d27
Egor A. Fedorov <efedorov@dataeast.com>
2025-01-16 16:06:08 +0700

    refactor: rename TokenRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
 
-import 'TokenRepository.dart';
+import 'token_repository.dart';
 
 void main() {
   runApp(const MyApp());
